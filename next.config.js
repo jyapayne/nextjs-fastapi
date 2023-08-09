44f3541316d3cc74c308a6d9db43d553f73a2dce
Joey Yakimowich-Payne <jyapayne@pm.me>
2023-08-09 10:09:29 -0600

    Update to latest code from vercel examples

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: '/api/:path*',
         destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/api/:path*'
+            : '/api/',
       },
       {
         source: "/docs",
@@ -25,6 +25,6 @@ const nextConfig = {
       },
     ];
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
